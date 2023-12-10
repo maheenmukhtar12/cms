df0b2aae4cf7a1fb37a47e53c479e90b01cc2718
maheenmukhtar12 <143339583+maheenmukhtar12@users.noreply.github.com>
2023-12-10 20:57:00 +0500

    Add files via upload

---
+++ includes/db.php
@@ -0,0 +1,14 @@
+<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'cms';
+
+$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+// if ($connection) {
+//     echo "We are connected";
+// }
+
+
+?>
